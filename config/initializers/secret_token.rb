8e2917737e1a7cc2cfe2a7ea76294c5e5bbb4025
Yan Namochenko <dierus@yandex.ru>
2015-11-03 04:42:00 +0200

    Ubuntu ver

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstAppUbuntu::Application.config.secret_key_base = '3b6412cc961063b20a3af20af8d2e706b0440ebc9ab61050a583d7e83a1218653b4d6ea74071c156690674f6ae1530383927e5bbb88a9c2f216c1450ceb1f537'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FirstAppUbuntu::Application.config.session_store :cookie_store, key: '_first_app_ubuntu_session'
